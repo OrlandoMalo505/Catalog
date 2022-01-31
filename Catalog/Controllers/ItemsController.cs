4cd84eab2ce87e6b1f2f7399990850d819548511
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-01-31 22:45:03 +0100

    added items controller

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+﻿using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("/items")]
+    public class ItemsController : ControllerBase
+    {
+
+        private readonly InMemItemsRepository _repository;
+
+        public ItemsController( )
+        {
+            _repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            return _repository.GetItems();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item= _repository.GetItemById(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
