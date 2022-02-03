2506b4ded8ac7754476176d33570565a5300fcea
Orlando Malo <Orlando.Malo@fti.edu.al>
2022-02-03 23:55:20 +0100

    added connection with mongo db

---
+++ Catalog/Repositories/MongoDbRepo.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbRepo : IInMemItemsRepository
+    {
+        private const string DatabaseName = "catalog";
+
+        private const string CollectionName = "items";
+
+        private readonly IMongoCollection<Item> itemsCollection;
+
+        public MongoDbRepo(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(DatabaseName);
+            itemsCollection = database.GetCollection<Item>(CollectionName);
+
+        }
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItemById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
